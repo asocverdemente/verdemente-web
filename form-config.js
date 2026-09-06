5af1c42d7a98cc209d257d33452be1c329d2b385
asocverdemente <asocverdemente@gmail.com>
2026-09-06 20:13:47 +0200

    Update form-config.js

--- form-config.js
+++ form-config.js
@@ -1,2 +1,2 @@
 // Pega entre las comillas la URL de implementación de tu Google Apps Script.
-window.VERDEMENTE_FORM_ENDPOINT = '';
+window.VERDEMENTE_FORM_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwllQ1nXzlmarS0NgOrPPf-ooB6dC3OsCIrbUkcOG5moceeLmGiFwzSHW0HUC2bqLItCw/exec';
